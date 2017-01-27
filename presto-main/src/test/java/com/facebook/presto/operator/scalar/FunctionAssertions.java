28d8a1d3abc28afe01ab8be48790e9eca6c509c4
Andrzej Fiedukowicz <Andrzej.Fiedukowicz@teradata.com>
2017-01-27 14:45:25 +0100

    Introduce new TIME/TIMESTAMP semantics to scalar functions

--- presto-main/src/test/java/com/facebook/presto/operator/scalar/FunctionAssertions.java
+++ presto-main/src/test/java/com/facebook/presto/operator/scalar/FunctionAssertions.java
@@ -821,6 +821,11 @@ public final class FunctionAssertions
         assertEquals(actualType, expectedType);
     }
 
+    public Session getSession()
+    {
+        return session;
+    }
+
     @Override
     public void close()
     {
