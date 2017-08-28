zhdk123456/presto

Implement SET ROLE in Hive Connector

presto-hive/src/main/java/com/facebook/presto/hive/metastore/MetastoreUtil.java
@@ -16,9 +16,11 @@ package com.facebook.presto.hive.metastore;
 import com.facebook.presto.hive.HiveBucketProperty;
 import com.facebook.presto.hive.HiveType;
 import com.facebook.presto.spi.PrestoException;
+import com.facebook.presto.spi.security.Identity;
 import com.facebook.presto.spi.security.PrestoPrincipal;
 import com.facebook.presto.spi.security.PrincipalType;
 import com.facebook.presto.spi.security.RoleGrant;
+import com.facebook.presto.spi.security.SelectedRole;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
 import org.apache.hadoop.hive.common.FileUtils;
@@ -41,6 +43,7 @@ import java.util.Properties;
 import java.util.Queue;
 import java.util.Set;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import static com.facebook.presto.hive.HiveErrorCode.HIVE_INVALID_METADATA;
 import static com.facebook.presto.spi.security.PrincipalType.ROLE;
@@ -394,6 +397,40 @@ public class MetastoreUtil
             }
         }
         return ImmutableSet.copyOf(result);
+    }
+
+    public static Set<String> listEnabledRoles(Identity identity, Function<PrestoPrincipal, Set<RoleGrant>> listRoleGrants)
+    {
+        Optional<SelectedRole> role = identity.getRole();
+
+        if (role.isPresent() && role.get().getType() == SelectedRole.Type.NONE) {
+            return ImmutableSet.of("public");
+        }
+
+        PrestoPrincipal principal;
+        if (!role.isPresent() || role.get().getType() == SelectedRole.Type.ALL) {
+            principal = new PrestoPrincipal(USER, identity.getUser());
+        }
+        else {
+            principal = new PrestoPrincipal(ROLE, identity.getRole().get().getRole().get());
+        }
+
+        Set<String> roles = listApplicableRoles(principal, listRoleGrants)
+                .stream()
+                .map(RoleGrant::getRoleName)
+                .collect(Collectors.toSet());
+
+        if (!principal.equals(new PrestoPrincipal(ROLE, "admin"))) {
+            roles.remove("admin");
+        }
+
+        roles.add("public");
+
+        if (principal.getType() == ROLE) {
+            roles.add(principal.getName());
+        }
+
+        return ImmutableSet.copyOf(roles);
     }
 
     private static PrincipalType fromMetastoreApiPrincipalType(org.apache.hadoop.hive.metastore.api.PrincipalType principalType)

presto-hive/src/main/java/com/facebook/presto/hive/security/SqlStandardAccessControl.java
@@ -68,6 +68,7 @@ import static com.facebook.presto.spi.security.AccessDeniedException.denyRevokeT
 import static com.facebook.presto.spi.security.AccessDeniedException.denySelectTable;
 import static com.facebook.presto.spi.security.AccessDeniedException.denySelectView;
 import static com.facebook.presto.spi.security.AccessDeniedException.denySetCatalogSessionProperty;
+import static com.facebook.presto.spi.security.AccessDeniedException.denySetRole;
 import static com.facebook.presto.spi.security.PrincipalType.ROLE;
 import static com.facebook.presto.spi.security.PrincipalType.USER;
 import static java.util.Objects.requireNonNull;
@@ -340,6 +341,15 @@ public class SqlStandardAccessControl
         }
         if (!hasAdminOption(transactionHandle, identity, roles)) {
             denyRevokeRoles(roles, grantees);
+        }
+    }
+
+    @Override
+    public void checkCanSetRole(ConnectorTransactionHandle transaction, Identity identity, String role, String catalogName)
+    {
+        SemiTransactionalHiveMetastore metastore = metastoreProvider.apply(((HiveTransactionHandle) transaction));
+        if (!listApplicableRoles(metastore, new PrestoPrincipal(USER, identity.getUser())).contains(role)) {
+            denySetRole(role);
         }
     }
 

sopel39/presto_td

presto-hive/src/main/java/com/facebook/presto/hive/metastore/MetastoreUtil.java
@@ -20,9 +20,11 @@ import com.facebook.presto.hive.TableOfflineException;
 import com.facebook.presto.spi.PrestoException;
 import com.facebook.presto.spi.SchemaTableName;
 import com.facebook.presto.spi.TableNotFoundException;
+import com.facebook.presto.spi.security.ConnectorIdentity;
 import com.facebook.presto.spi.security.PrestoPrincipal;
 import com.facebook.presto.spi.security.PrincipalType;
 import com.facebook.presto.spi.security.RoleGrant;
+import com.facebook.presto.spi.security.SelectedRole;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
 import org.apache.hadoop.hive.common.FileUtils;
@@ -63,6 +65,7 @@ import java.util.Properties;
 import java.util.Queue;
 import java.util.Set;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import static com.facebook.presto.hive.HiveErrorCode.HIVE_INVALID_METADATA;
 import static com.facebook.presto.hive.HiveSplitManager.PRESTO_OFFLINE;
@@ -548,6 +551,40 @@ public class MetastoreUtil
             result.addAll(metastore.listTablePrivileges(databaseName, tableName, current));
         }
         return result.build();
+    }
+
+    public static Set<String> listEnabledRoles(ConnectorIdentity identity, Function<PrestoPrincipal, Set<RoleGrant>> listRoleGrants)
+    {
+        Optional<SelectedRole> role = identity.getRole();
+
+        if (role.isPresent() && role.get().getType() == SelectedRole.Type.NONE) {
+            return ImmutableSet.of("public");
+        }
+
+        PrestoPrincipal principal;
+        if (!role.isPresent() || role.get().getType() == SelectedRole.Type.ALL) {
+            principal = new PrestoPrincipal(USER, identity.getUser());
+        }
+        else {
+            principal = new PrestoPrincipal(ROLE, identity.getRole().get().getRole().get());
+        }
+
+        Set<String> roles = listApplicableRoles(principal, listRoleGrants)
+                .stream()
+                .map(RoleGrant::getRoleName)
+                .collect(Collectors.toSet());
+
+        if (!principal.equals(new PrestoPrincipal(ROLE, "admin"))) {
+            roles.remove("admin");
+        }
+
+        roles.add("public");
+
+        if (principal.getType() == ROLE) {
+            roles.add(principal.getName());
+        }
+
+        return ImmutableSet.copyOf(roles);
     }
 
     private static PrincipalType fromMetastoreApiPrincipalType(org.apache.hadoop.hive.metastore.api.PrincipalType principalType)

presto-hive/src/main/java/com/facebook/presto/hive/security/SqlStandardAccessControl.java
@@ -68,6 +68,7 @@ import static com.facebook.presto.spi.security.AccessDeniedException.denyRevokeT
 import static com.facebook.presto.spi.security.AccessDeniedException.denySelectTable;
 import static com.facebook.presto.spi.security.AccessDeniedException.denySelectView;
 import static com.facebook.presto.spi.security.AccessDeniedException.denySetCatalogSessionProperty;
+import static com.facebook.presto.spi.security.AccessDeniedException.denySetRole;
 import static com.facebook.presto.spi.security.PrincipalType.ROLE;
 import static com.facebook.presto.spi.security.PrincipalType.USER;
 import static java.util.Objects.requireNonNull;
@@ -342,6 +343,15 @@ public class SqlStandardAccessControl
         }
         if (!hasAdminOptionForRoles(transactionHandle, identity, roles)) {
             denyRevokeRoles(roles, grantees);
+        }
+    }
+
+    @Override
+    public void checkCanSetRole(ConnectorTransactionHandle transaction, ConnectorIdentity identity, String role, String catalogName)
+    {
+        SemiTransactionalHiveMetastore metastore = metastoreProvider.apply(((HiveTransactionHandle) transaction));
+        if (!listApplicableRoles(metastore, new PrestoPrincipal(USER, identity.getUser())).contains(role)) {
+            denySetRole(role);
         }
     }
 