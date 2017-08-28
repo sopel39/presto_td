Teradata/presto

Introduce isTableOwner method for readability

presto-hive/src/main/java/com/facebook/presto/hive/security/SqlStandardAccessControl.java
@@ -146,7 +146,7 @@ public class SqlStandardAccessControl
     @Override
     public void checkCanDropTable(ConnectorTransactionHandle transaction, Identity identity, SchemaTableName tableName)
     {
-        if (!checkTablePermission(transaction, identity, tableName, OWNERSHIP)) {
+        if (!isTableOwner(transaction, identity, tableName)) {
             denyDropTable(tableName.toString());
         }
     }
@@ -154,7 +154,7 @@ public class SqlStandardAccessControl
     @Override
     public void checkCanRenameTable(ConnectorTransactionHandle transaction, Identity identity, SchemaTableName tableName, SchemaTableName newTableName)
     {
-        if (!checkTablePermission(transaction, identity, tableName, OWNERSHIP)) {
+        if (!isTableOwner(transaction, identity, tableName)) {
             denyRenameTable(tableName.toString(), newTableName.toString());
         }
     }
@@ -173,7 +173,7 @@ public class SqlStandardAccessControl
     @Override
     public void checkCanAddColumn(ConnectorTransactionHandle transaction, Identity identity, SchemaTableName tableName)
     {
-        if (!checkTablePermission(transaction, identity, tableName, OWNERSHIP)) {
+        if (!isTableOwner(transaction, identity, tableName)) {
             denyAddColumn(tableName.toString());
         }
     }
@@ -181,7 +181,7 @@ public class SqlStandardAccessControl
     @Override
     public void checkCanRenameColumn(ConnectorTransactionHandle transaction, Identity identity, SchemaTableName tableName)
     {
-        if (!checkTablePermission(transaction, identity, tableName, OWNERSHIP)) {
+        if (!isTableOwner(transaction, identity, tableName)) {
             denyRenameColumn(tableName.toString());
         }
     }
@@ -221,7 +221,7 @@ public class SqlStandardAccessControl
     @Override
     public void checkCanDropView(ConnectorTransactionHandle transaction, Identity identity, SchemaTableName viewName)
     {
-        if (!checkTablePermission(transaction, identity, viewName, OWNERSHIP)) {
+        if (!isTableOwner(transaction, identity, viewName)) {
             denyDropView(viewName.toString());
         }
     }
@@ -272,7 +272,7 @@ public class SqlStandardAccessControl
     @Override
     public void checkCanGrantTablePrivilege(ConnectorTransactionHandle transaction, Identity identity, Privilege privilege, SchemaTableName tableName)
     {
-        if (checkTablePermission(transaction, identity, tableName, OWNERSHIP)) {
+        if (isTableOwner(transaction, identity, tableName)) {
             return;
         }
 
@@ -284,7 +284,7 @@ public class SqlStandardAccessControl
     @Override
     public void checkCanRevokeTablePrivilege(ConnectorTransactionHandle transaction, Identity identity, Privilege privilege, SchemaTableName tableName)
     {
-        if (checkTablePermission(transaction, identity, tableName, OWNERSHIP)) {
+        if (isTableOwner(transaction, identity, tableName)) {
             return;
         }
 
@@ -414,6 +414,11 @@ public class SqlStandardAccessControl
             return true;
         }
         return false;
+    }
+
+    private boolean isTableOwner(ConnectorTransactionHandle transaction, Identity identity, SchemaTableName tableName)
+    {
+        return checkTablePermission(transaction, identity, tableName, OWNERSHIP);
     }
 
     private boolean checkTablePermission(ConnectorTransactionHandle transaction, Identity identity, SchemaTableName tableName, HivePrivilege... requiredPrivileges)

sopel39/presto_td

presto-hive/src/main/java/com/facebook/presto/hive/security/SqlStandardAccessControl.java
@@ -145,7 +145,7 @@ public class SqlStandardAccessControl
     @Override
     public void checkCanDropTable(ConnectorTransactionHandle transaction, ConnectorIdentity identity, SchemaTableName tableName)
     {
-        if (!checkTablePermission(transaction, identity, tableName, OWNERSHIP)) {
+        if (!isTableOwner(transaction, identity, tableName)) {
             denyDropTable(tableName.toString());
         }
     }
@@ -153,7 +153,7 @@ public class SqlStandardAccessControl
     @Override
     public void checkCanRenameTable(ConnectorTransactionHandle transaction, ConnectorIdentity identity, SchemaTableName tableName, SchemaTableName newTableName)
     {
-        if (!checkTablePermission(transaction, identity, tableName, OWNERSHIP)) {
+        if (!isTableOwner(transaction, identity, tableName)) {
             denyRenameTable(tableName.toString(), newTableName.toString());
         }
     }
@@ -172,7 +172,7 @@ public class SqlStandardAccessControl
     @Override
     public void checkCanAddColumn(ConnectorTransactionHandle transaction, ConnectorIdentity identity, SchemaTableName tableName)
     {
-        if (!checkTablePermission(transaction, identity, tableName, OWNERSHIP)) {
+        if (!isTableOwner(transaction, identity, tableName)) {
             denyAddColumn(tableName.toString());
         }
     }
@@ -180,7 +180,7 @@ public class SqlStandardAccessControl
     @Override
     public void checkCanDropColumn(ConnectorTransactionHandle transaction, ConnectorIdentity identity, SchemaTableName tableName)
     {
-        if (!checkTablePermission(transaction, identity, tableName, OWNERSHIP)) {
+        if (!isTableOwner(transaction, identity, tableName)) {
             denyDropColumn(tableName.toString());
         }
     }
@@ -188,7 +188,7 @@ public class SqlStandardAccessControl
     @Override
     public void checkCanRenameColumn(ConnectorTransactionHandle transaction, ConnectorIdentity identity, SchemaTableName tableName)
     {
-        if (!checkTablePermission(transaction, identity, tableName, OWNERSHIP)) {
+        if (!isTableOwner(transaction, identity, tableName)) {
             denyRenameColumn(tableName.toString());
         }
     }
@@ -228,7 +228,7 @@ public class SqlStandardAccessControl
     @Override
     public void checkCanDropView(ConnectorTransactionHandle transaction, ConnectorIdentity identity, SchemaTableName viewName)
     {
-        if (!checkTablePermission(transaction, identity, viewName, OWNERSHIP)) {
+        if (!isTableOwner(transaction, identity, viewName)) {
             denyDropView(viewName.toString());
         }
     }
@@ -279,7 +279,7 @@ public class SqlStandardAccessControl
     @Override
     public void checkCanGrantTablePrivilege(ConnectorTransactionHandle transaction, ConnectorIdentity identity, Privilege privilege, SchemaTableName tableName, PrestoPrincipal grantee, boolean withGrantOption)
     {
-        if (checkTablePermission(transaction, identity, tableName, OWNERSHIP)) {
+        if (isTableOwner(transaction, identity, tableName)) {
             return;
         }
 
@@ -291,7 +291,7 @@ public class SqlStandardAccessControl
     @Override
     public void checkCanRevokeTablePrivilege(ConnectorTransactionHandle transaction, ConnectorIdentity identity, Privilege privilege, SchemaTableName tableName, PrestoPrincipal revokee, boolean grantOptionFor)
     {
-        if (checkTablePermission(transaction, identity, tableName, OWNERSHIP)) {
+        if (isTableOwner(transaction, identity, tableName)) {
             return;
         }
 
@@ -410,6 +410,11 @@ public class SqlStandardAccessControl
             return true;
         }
         return false;
+    }
+
+    private boolean isTableOwner(ConnectorTransactionHandle transaction, ConnectorIdentity identity, SchemaTableName tableName)
+    {
+        return checkTablePermission(transaction, identity, tableName, OWNERSHIP);
     }
 
     private boolean checkTablePermission(ConnectorTransactionHandle transaction, ConnectorIdentity identity, SchemaTableName tableName, HivePrivilege... requiredPrivileges)