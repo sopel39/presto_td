windofthesky/presto

Add SHOW ROLES to the parser

presto-parser/src/main/java/com/facebook/presto/sql/parser/AstBuilder.java
@@ -128,6 +128,7 @@ import com.facebook.presto.sql.tree.ShowCreate;
 import com.facebook.presto.sql.tree.ShowFunctions;
 import com.facebook.presto.sql.tree.ShowGrants;
 import com.facebook.presto.sql.tree.ShowPartitions;
+import com.facebook.presto.sql.tree.ShowRoles;
 import com.facebook.presto.sql.tree.ShowSchemas;
 import com.facebook.presto.sql.tree.ShowSession;
 import com.facebook.presto.sql.tree.ShowStats;
@@ -907,6 +908,14 @@ class AstBuilder
                 context.TABLE() != null,
                 tableName,
                 context.ALL() != null);
+    }
+
+    @Override
+    public Node visitShowRoles(SqlBaseParser.ShowRolesContext context)
+    {
+        return new ShowRoles(
+                getLocation(context),
+                getTextIfPresent(context.identifier()));
     }
 
     // ***************** boolean expressions ******************

presto-parser/src/main/java/com/facebook/presto/sql/tree/ShowRoles.java
@@ -1,0 +1,89 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.sql.tree;
+
+import com.google.common.collect.ImmutableList;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+import static com.google.common.base.MoreObjects.toStringHelper;
+import static java.util.Objects.requireNonNull;
+
+public class ShowRoles
+    extends Statement
+{
+    private final Optional<String> catalog;
+
+    public ShowRoles(Optional<String> catalog)
+    {
+        this(Optional.empty(), catalog);
+    }
+
+    public ShowRoles(NodeLocation location, Optional<String> catalog)
+    {
+        this(Optional.of(location), catalog);
+    }
+
+    public ShowRoles(Optional<NodeLocation> location, Optional<String> catalog)
+    {
+        super(location);
+        this.catalog = requireNonNull(catalog, "catalog is null");
+    }
+
+    public Optional<String> getCatalog()
+    {
+        return catalog;
+    }
+
+    @Override
+    public <R, C> R accept(AstVisitor<R, C> visitor, C context)
+    {
+        return visitor.visitShowRoles(this, context);
+    }
+
+    @Override
+    public List<Node> getChildren()
+    {
+        return ImmutableList.of();
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return Objects.hash(catalog);
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        if (this == obj) {
+            return true;
+        }
+        if ((obj == null) || (getClass() != obj.getClass())) {
+            return false;
+        }
+        ShowRoles o = (ShowRoles) obj;
+        return Objects.equals(catalog, o.catalog);
+    }
+
+    @Override
+    public String toString()
+    {
+        return toStringHelper(this)
+                .add("catalog", catalog)
+                .toString();
+    }
+}

sopel39/presto_td

presto-parser/src/main/java/com/facebook/presto/sql/parser/AstBuilder.java
@@ -131,6 +131,7 @@ import com.facebook.presto.sql.tree.ShowCreate;
 import com.facebook.presto.sql.tree.ShowFunctions;
 import com.facebook.presto.sql.tree.ShowGrants;
 import com.facebook.presto.sql.tree.ShowPartitions;
+import com.facebook.presto.sql.tree.ShowRoles;
 import com.facebook.presto.sql.tree.ShowSchemas;
 import com.facebook.presto.sql.tree.ShowSession;
 import com.facebook.presto.sql.tree.ShowStats;
@@ -912,6 +913,14 @@ class AstBuilder
                 getLocation(context),
                 context.TABLE() != null,
                 tableName);
+    }
+
+    @Override
+    public Node visitShowRoles(SqlBaseParser.ShowRolesContext context)
+    {
+        return new ShowRoles(
+                getLocation(context),
+                getIdentifierIfPresent(context.identifier()));
     }
 
     // ***************** boolean expressions ******************

presto-parser/src/main/java/com/facebook/presto/sql/tree/ShowRoles.java
@@ -1,0 +1,89 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.sql.tree;
+
+import com.google.common.collect.ImmutableList;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+import static com.google.common.base.MoreObjects.toStringHelper;
+import static java.util.Objects.requireNonNull;
+
+public class ShowRoles
+        extends Statement
+{
+    private final Optional<Identifier> catalog;
+
+    public ShowRoles(Optional<Identifier> catalog)
+    {
+        this(Optional.empty(), catalog);
+    }
+
+    public ShowRoles(NodeLocation location, Optional<Identifier> catalog)
+    {
+        this(Optional.of(location), catalog);
+    }
+
+    public ShowRoles(Optional<NodeLocation> location, Optional<Identifier> catalog)
+    {
+        super(location);
+        this.catalog = requireNonNull(catalog, "catalog is null");
+    }
+
+    public Optional<Identifier> getCatalog()
+    {
+        return catalog;
+    }
+
+    @Override
+    public <R, C> R accept(AstVisitor<R, C> visitor, C context)
+    {
+        return visitor.visitShowRoles(this, context);
+    }
+
+    @Override
+    public List<Node> getChildren()
+    {
+        return ImmutableList.of();
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return Objects.hash(catalog);
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        if (this == obj) {
+            return true;
+        }
+        if ((obj == null) || (getClass() != obj.getClass())) {
+            return false;
+        }
+        ShowRoles o = (ShowRoles) obj;
+        return Objects.equals(catalog, o.catalog);
+    }
+
+    @Override
+    public String toString()
+    {
+        return toStringHelper(this)
+                .add("catalog", catalog)
+                .toString();
+    }
+}