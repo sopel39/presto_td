zhdk123456/presto

Introduce GRANT/REVOKE roles statements

presto-parser/src/main/java/com/facebook/presto/sql/SqlFormatter.java
@@ -42,6 +42,7 @@ import com.facebook.presto.sql.tree.ExplainOption;
 import com.facebook.presto.sql.tree.ExplainType;
 import com.facebook.presto.sql.tree.Expression;
 import com.facebook.presto.sql.tree.Grant;
+import com.facebook.presto.sql.tree.GrantRoles;
 import com.facebook.presto.sql.tree.GrantorSpecification;
 import com.facebook.presto.sql.tree.Insert;
 import com.facebook.presto.sql.tree.Intersect;
@@ -65,6 +66,7 @@ import com.facebook.presto.sql.tree.RenameSchema;
 import com.facebook.presto.sql.tree.RenameTable;
 import com.facebook.presto.sql.tree.ResetSession;
 import com.facebook.presto.sql.tree.Revoke;
+import com.facebook.presto.sql.tree.RevokeRoles;
 import com.facebook.presto.sql.tree.Rollback;
 import com.facebook.presto.sql.tree.Row;
 import com.facebook.presto.sql.tree.SampledRelation;
@@ -1048,6 +1050,68 @@ public final class SqlFormatter
         protected Void visitDropRole(DropRole node, Integer context)
         {
             builder.append("DROP ROLE ").append(formatName(node.getName()));
+            if (node.getCatalog().isPresent()) {
+                builder.append(" IN ").append(formatName(node.getCatalog().get()));
+            }
+            return null;
+        }
+
+        @Override
+        protected Void visitGrantRoles(GrantRoles node, Integer context)
+        {
+            builder.append("GRANT ");
+            Iterator<String> roles = node.getRoles().iterator();
+            while (roles.hasNext()) {
+                builder.append(formatName(roles.next()));
+                if (roles.hasNext()) {
+                    builder.append(", ");
+                }
+            }
+            builder.append(" TO ");
+            Iterator<PrincipalSpecification> grantees = node.getGrantees().iterator();
+            while (grantees.hasNext()) {
+                builder.append(formatPrincipal(grantees.next()));
+                if (grantees.hasNext()) {
+                    builder.append(", ");
+                }
+            }
+            if (node.isWithAdminOption()) {
+                builder.append(" WITH ADMIN OPTION");
+            }
+            if (node.getGrantor().isPresent()) {
+                builder.append(" GRANTED BY ").append(formatGrantor(node.getGrantor().get()));
+            }
+            if (node.getCatalog().isPresent()) {
+                builder.append(" IN ").append(formatName(node.getCatalog().get()));
+            }
+            return null;
+        }
+
+        @Override
+        protected Void visitRevokeRoles(RevokeRoles node, Integer context)
+        {
+            builder.append("REVOKE ");
+            if (node.isAdminOptionFor()) {
+                builder.append("ADMIN OPTION FOR ");
+            }
+            Iterator<String> roles = node.getRoles().iterator();
+            while (roles.hasNext()) {
+                builder.append(formatName(roles.next()));
+                if (roles.hasNext()) {
+                    builder.append(", ");
+                }
+            }
+            builder.append(" FROM ");
+            Iterator<PrincipalSpecification> grantees = node.getGrantees().iterator();
+            while (grantees.hasNext()) {
+                builder.append(formatPrincipal(grantees.next()));
+                if (grantees.hasNext()) {
+                    builder.append(", ");
+                }
+            }
+            if (node.getGrantor().isPresent()) {
+                builder.append(" GRANTED BY ").append(formatGrantor(node.getGrantor().get()));
+            }
             if (node.getCatalog().isPresent()) {
                 builder.append(" IN ").append(formatName(node.getCatalog().get()));
             }

presto-parser/src/main/java/com/facebook/presto/sql/parser/AstBuilder.java
@@ -65,6 +65,7 @@ import com.facebook.presto.sql.tree.FrameBound;
 import com.facebook.presto.sql.tree.FunctionCall;
 import com.facebook.presto.sql.tree.GenericLiteral;
 import com.facebook.presto.sql.tree.Grant;
+import com.facebook.presto.sql.tree.GrantRoles;
 import com.facebook.presto.sql.tree.GrantorSpecification;
 import com.facebook.presto.sql.tree.GroupBy;
 import com.facebook.presto.sql.tree.GroupingElement;
@@ -110,6 +111,7 @@ import com.facebook.presto.sql.tree.RenameSchema;
 import com.facebook.presto.sql.tree.RenameTable;
 import com.facebook.presto.sql.tree.ResetSession;
 import com.facebook.presto.sql.tree.Revoke;
+import com.facebook.presto.sql.tree.RevokeRoles;
 import com.facebook.presto.sql.tree.Rollback;
 import com.facebook.presto.sql.tree.Rollup;
 import com.facebook.presto.sql.tree.Row;
@@ -154,6 +156,7 @@ import com.facebook.presto.sql.tree.With;
 import com.facebook.presto.sql.tree.WithQuery;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Lists;
 import org.antlr.v4.runtime.ParserRuleContext;
 import org.antlr.v4.runtime.Token;
@@ -723,6 +726,30 @@ class AstBuilder
     public Node visitDropRole(SqlBaseParser.DropRoleContext context)
     {
         return new DropRole(getLocation(context), context.name.getText(), getTextIfPresent(context.catalog));
+    }
+
+    @Override
+    public Node visitGrantRoles(SqlBaseParser.GrantRolesContext context)
+    {
+        return new GrantRoles(
+                getLocation(context),
+                ImmutableSet.copyOf(getIdentifiers(context.roles().identifier())),
+                ImmutableSet.copyOf(getPrincipalSpecifications(context.principal())),
+                context.OPTION() != null,
+                getGrantorSpecificationIfPresent(context.grantor()),
+                getTextIfPresent(context.catalog));
+    }
+
+    @Override
+    public Node visitRevokeRoles(SqlBaseParser.RevokeRolesContext context)
+    {
+        return new RevokeRoles(
+                getLocation(context),
+                ImmutableSet.copyOf(getIdentifiers(context.roles().identifier())),
+                ImmutableSet.copyOf(getPrincipalSpecifications(context.principal())),
+                context.OPTION() != null,
+                getGrantorSpecificationIfPresent(context.grantor()),
+                getTextIfPresent(context.catalog));
     }
 
     @Override
@@ -1789,6 +1816,16 @@ class AstBuilder
             return getType(typeParameter.type());
         }
         throw new IllegalArgumentException("Unsupported typeParameter: " + typeParameter.getText());
+    }
+
+    private static List<String> getIdentifiers(List<SqlBaseParser.IdentifierContext> identifiers)
+    {
+        return identifiers.stream().map(SqlBaseParser.IdentifierContext::getText).collect(toList());
+    }
+
+    private static List<PrincipalSpecification> getPrincipalSpecifications(List<SqlBaseParser.PrincipalContext> principals)
+    {
+        return principals.stream().map(AstBuilder::getPrincipalSpecification).collect(toList());
     }
 
     private static Optional<GrantorSpecification> getGrantorSpecificationIfPresent(SqlBaseParser.GrantorContext context)

presto-parser/src/main/java/com/facebook/presto/sql/tree/RevokeRoles.java
@@ -1,0 +1,146 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.sql.tree;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Set;
+
+import static com.google.common.base.MoreObjects.toStringHelper;
+import static java.util.Locale.ENGLISH;
+import static java.util.Objects.requireNonNull;
+import static java.util.stream.Collectors.toSet;
+
+public class RevokeRoles
+        extends Statement
+{
+    private final Set<String> roles;
+    private final Set<PrincipalSpecification> grantees;
+    private final boolean adminOptionFor;
+    private final Optional<GrantorSpecification> grantor;
+    private final Optional<String> catalog;
+
+    public RevokeRoles(
+            NodeLocation location,
+            Set<String> roles,
+            Set<PrincipalSpecification> grantees,
+            boolean adminOptionFor,
+            Optional<GrantorSpecification> grantor,
+            Optional<String> catalog)
+    {
+        this(Optional.of(location), roles, grantees, adminOptionFor, grantor, catalog);
+    }
+
+    public RevokeRoles(
+            Set<String> roles,
+            Set<PrincipalSpecification> grantees,
+            boolean adminOptionFor,
+            Optional<GrantorSpecification> grantor,
+            Optional<String> catalog)
+    {
+        this(Optional.empty(), roles, grantees, adminOptionFor, grantor, catalog);
+    }
+
+    private RevokeRoles(
+            Optional<NodeLocation> location,
+            Set<String> roles,
+            Set<PrincipalSpecification> grantees,
+            boolean adminOptionFor,
+            Optional<GrantorSpecification> grantor,
+            Optional<String> catalog)
+    {
+        super(location);
+        this.roles = ImmutableSet.copyOf(requireNonNull(roles, "roles is null").stream().map(r -> r.toLowerCase(ENGLISH)).collect(toSet()));
+        this.grantees = ImmutableSet.copyOf(requireNonNull(grantees, "grantees is null"));
+        this.adminOptionFor = adminOptionFor;
+        this.grantor = requireNonNull(grantor, "grantor is null");
+        this.catalog = requireNonNull(catalog, "catalog is null").map(c -> c.toLowerCase(ENGLISH));
+    }
+
+    public Set<String> getRoles()
+    {
+        return roles;
+    }
+
+    public Set<PrincipalSpecification> getGrantees()
+    {
+        return grantees;
+    }
+
+    public boolean isAdminOptionFor()
+    {
+        return adminOptionFor;
+    }
+
+    public Optional<GrantorSpecification> getGrantor()
+    {
+        return grantor;
+    }
+
+    public Optional<String> getCatalog()
+    {
+        return catalog;
+    }
+
+    @Override
+    public List<? extends Node> getChildren()
+    {
+        return ImmutableList.of();
+    }
+
+    @Override
+    public <R, C> R accept(AstVisitor<R, C> visitor, C context)
+    {
+        return visitor.visitRevokeRoles(this, context);
+    }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        RevokeRoles that = (RevokeRoles) o;
+        return adminOptionFor == that.adminOptionFor &&
+                Objects.equals(roles, that.roles) &&
+                Objects.equals(grantees, that.grantees) &&
+                Objects.equals(grantor, that.grantor) &&
+                Objects.equals(catalog, that.catalog);
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return Objects.hash(roles, grantees, adminOptionFor, grantor, catalog);
+    }
+
+    @Override
+    public String toString()
+    {
+        return toStringHelper(this)
+                .add("roles", roles)
+                .add("grantees", grantees)
+                .add("adminOptionFor", adminOptionFor)
+                .add("grantor", grantor)
+                .add("catalog", catalog)
+                .toString();
+    }
+}

sopel39/presto_td

presto-parser/src/main/java/com/facebook/presto/sql/SqlFormatter.java
@@ -43,6 +43,7 @@ import com.facebook.presto.sql.tree.ExplainOption;
 import com.facebook.presto.sql.tree.ExplainType;
 import com.facebook.presto.sql.tree.Expression;
 import com.facebook.presto.sql.tree.Grant;
+import com.facebook.presto.sql.tree.GrantRoles;
 import com.facebook.presto.sql.tree.GrantorSpecification;
 import com.facebook.presto.sql.tree.Identifier;
 import com.facebook.presto.sql.tree.Insert;
@@ -68,6 +69,7 @@ import com.facebook.presto.sql.tree.RenameSchema;
 import com.facebook.presto.sql.tree.RenameTable;
 import com.facebook.presto.sql.tree.ResetSession;
 import com.facebook.presto.sql.tree.Revoke;
+import com.facebook.presto.sql.tree.RevokeRoles;
 import com.facebook.presto.sql.tree.Rollback;
 import com.facebook.presto.sql.tree.Row;
 import com.facebook.presto.sql.tree.SampledRelation;
@@ -1096,6 +1098,68 @@ public final class SqlFormatter
         protected Void visitDropRole(DropRole node, Integer context)
         {
             builder.append("DROP ROLE ").append(node.getName());
+            if (node.getCatalog().isPresent()) {
+                builder.append(" IN ").append(node.getCatalog().get());
+            }
+            return null;
+        }
+
+        @Override
+        protected Void visitGrantRoles(GrantRoles node, Integer context)
+        {
+            builder.append("GRANT ");
+            Iterator<Identifier> roles = node.getRoles().iterator();
+            while (roles.hasNext()) {
+                builder.append(roles.next());
+                if (roles.hasNext()) {
+                    builder.append(", ");
+                }
+            }
+            builder.append(" TO ");
+            Iterator<PrincipalSpecification> grantees = node.getGrantees().iterator();
+            while (grantees.hasNext()) {
+                builder.append(formatPrincipal(grantees.next()));
+                if (grantees.hasNext()) {
+                    builder.append(", ");
+                }
+            }
+            if (node.isWithAdminOption()) {
+                builder.append(" WITH ADMIN OPTION");
+            }
+            if (node.getGrantor().isPresent()) {
+                builder.append(" GRANTED BY ").append(formatGrantor(node.getGrantor().get()));
+            }
+            if (node.getCatalog().isPresent()) {
+                builder.append(" IN ").append(node.getCatalog().get());
+            }
+            return null;
+        }
+
+        @Override
+        protected Void visitRevokeRoles(RevokeRoles node, Integer context)
+        {
+            builder.append("REVOKE ");
+            if (node.isAdminOptionFor()) {
+                builder.append("ADMIN OPTION FOR ");
+            }
+            Iterator<Identifier> roles = node.getRoles().iterator();
+            while (roles.hasNext()) {
+                builder.append(roles.next());
+                if (roles.hasNext()) {
+                    builder.append(", ");
+                }
+            }
+            builder.append(" FROM ");
+            Iterator<PrincipalSpecification> grantees = node.getGrantees().iterator();
+            while (grantees.hasNext()) {
+                builder.append(formatPrincipal(grantees.next()));
+                if (grantees.hasNext()) {
+                    builder.append(", ");
+                }
+            }
+            if (node.getGrantor().isPresent()) {
+                builder.append(" GRANTED BY ").append(formatGrantor(node.getGrantor().get()));
+            }
             if (node.getCatalog().isPresent()) {
                 builder.append(" IN ").append(node.getCatalog().get());
             }

presto-parser/src/main/java/com/facebook/presto/sql/parser/AstBuilder.java
@@ -67,6 +67,7 @@ import com.facebook.presto.sql.tree.FrameBound;
 import com.facebook.presto.sql.tree.FunctionCall;
 import com.facebook.presto.sql.tree.GenericLiteral;
 import com.facebook.presto.sql.tree.Grant;
+import com.facebook.presto.sql.tree.GrantRoles;
 import com.facebook.presto.sql.tree.GrantorSpecification;
 import com.facebook.presto.sql.tree.GroupBy;
 import com.facebook.presto.sql.tree.GroupingElement;
@@ -114,6 +115,7 @@ import com.facebook.presto.sql.tree.RenameSchema;
 import com.facebook.presto.sql.tree.RenameTable;
 import com.facebook.presto.sql.tree.ResetSession;
 import com.facebook.presto.sql.tree.Revoke;
+import com.facebook.presto.sql.tree.RevokeRoles;
 import com.facebook.presto.sql.tree.Rollback;
 import com.facebook.presto.sql.tree.Rollup;
 import com.facebook.presto.sql.tree.Row;
@@ -160,6 +162,7 @@ import com.facebook.presto.sql.tree.With;
 import com.facebook.presto.sql.tree.WithQuery;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Lists;
 import org.antlr.v4.runtime.ParserRuleContext;
 import org.antlr.v4.runtime.Token;
@@ -813,6 +816,30 @@ class AstBuilder
         return new DropRole(
                 getLocation(context),
                 (Identifier) visit(context.name),
+                getIdentifierIfPresent(context.catalog));
+    }
+
+    @Override
+    public Node visitGrantRoles(SqlBaseParser.GrantRolesContext context)
+    {
+        return new GrantRoles(
+                getLocation(context),
+                ImmutableSet.copyOf(getIdentifiers(context.roles().identifier())),
+                ImmutableSet.copyOf(getPrincipalSpecifications(context.principal())),
+                context.OPTION() != null,
+                getGrantorSpecificationIfPresent(context.grantor()),
+                getIdentifierIfPresent(context.catalog));
+    }
+
+    @Override
+    public Node visitRevokeRoles(SqlBaseParser.RevokeRolesContext context)
+    {
+        return new RevokeRoles(
+                getLocation(context),
+                ImmutableSet.copyOf(getIdentifiers(context.roles().identifier())),
+                ImmutableSet.copyOf(getPrincipalSpecifications(context.principal())),
+                context.OPTION() != null,
+                getGrantorSpecificationIfPresent(context.grantor()),
                 getIdentifierIfPresent(context.catalog));
     }
 
@@ -2060,6 +2087,16 @@ class AstBuilder
             return getType(typeParameter.type());
         }
         throw new IllegalArgumentException("Unsupported typeParameter: " + typeParameter.getText());
+    }
+
+    private List<Identifier> getIdentifiers(List<SqlBaseParser.IdentifierContext> identifiers)
+    {
+        return identifiers.stream().map(context -> (Identifier) visit(context)).collect(toList());
+    }
+
+    private List<PrincipalSpecification> getPrincipalSpecifications(List<SqlBaseParser.PrincipalContext> principals)
+    {
+        return principals.stream().map(this::getPrincipalSpecification).collect(toList());
     }
 
     private Optional<GrantorSpecification> getGrantorSpecificationIfPresent(SqlBaseParser.GrantorContext context)

presto-parser/src/main/java/com/facebook/presto/sql/tree/RevokeRoles.java
@@ -1,0 +1,144 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.sql.tree;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Set;
+
+import static com.google.common.base.MoreObjects.toStringHelper;
+import static java.util.Objects.requireNonNull;
+
+public class RevokeRoles
+        extends Statement
+{
+    private final Set<Identifier> roles;
+    private final Set<PrincipalSpecification> grantees;
+    private final boolean adminOptionFor;
+    private final Optional<GrantorSpecification> grantor;
+    private final Optional<Identifier> catalog;
+
+    public RevokeRoles(
+            NodeLocation location,
+            Set<Identifier> roles,
+            Set<PrincipalSpecification> grantees,
+            boolean adminOptionFor,
+            Optional<GrantorSpecification> grantor,
+            Optional<Identifier> catalog)
+    {
+        this(Optional.of(location), roles, grantees, adminOptionFor, grantor, catalog);
+    }
+
+    public RevokeRoles(
+            Set<Identifier> roles,
+            Set<PrincipalSpecification> grantees,
+            boolean adminOptionFor,
+            Optional<GrantorSpecification> grantor,
+            Optional<Identifier> catalog)
+    {
+        this(Optional.empty(), roles, grantees, adminOptionFor, grantor, catalog);
+    }
+
+    private RevokeRoles(
+            Optional<NodeLocation> location,
+            Set<Identifier> roles,
+            Set<PrincipalSpecification> grantees,
+            boolean adminOptionFor,
+            Optional<GrantorSpecification> grantor,
+            Optional<Identifier> catalog)
+    {
+        super(location);
+        this.roles = ImmutableSet.copyOf(requireNonNull(roles, "roles is null"));
+        this.grantees = ImmutableSet.copyOf(requireNonNull(grantees, "grantees is null"));
+        this.adminOptionFor = adminOptionFor;
+        this.grantor = requireNonNull(grantor, "grantor is null");
+        this.catalog = requireNonNull(catalog, "catalog is null");
+    }
+
+    public Set<Identifier> getRoles()
+    {
+        return roles;
+    }
+
+    public Set<PrincipalSpecification> getGrantees()
+    {
+        return grantees;
+    }
+
+    public boolean isAdminOptionFor()
+    {
+        return adminOptionFor;
+    }
+
+    public Optional<GrantorSpecification> getGrantor()
+    {
+        return grantor;
+    }
+
+    public Optional<Identifier> getCatalog()
+    {
+        return catalog;
+    }
+
+    @Override
+    public List<? extends Node> getChildren()
+    {
+        return ImmutableList.of();
+    }
+
+    @Override
+    public <R, C> R accept(AstVisitor<R, C> visitor, C context)
+    {
+        return visitor.visitRevokeRoles(this, context);
+    }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        RevokeRoles that = (RevokeRoles) o;
+        return adminOptionFor == that.adminOptionFor &&
+                Objects.equals(roles, that.roles) &&
+                Objects.equals(grantees, that.grantees) &&
+                Objects.equals(grantor, that.grantor) &&
+                Objects.equals(catalog, that.catalog);
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return Objects.hash(roles, grantees, adminOptionFor, grantor, catalog);
+    }
+
+    @Override
+    public String toString()
+    {
+        return toStringHelper(this)
+                .add("roles", roles)
+                .add("grantees", grantees)
+                .add("adminOptionFor", adminOptionFor)
+                .add("grantor", grantor)
+                .add("catalog", catalog)
+                .toString();
+    }
+}