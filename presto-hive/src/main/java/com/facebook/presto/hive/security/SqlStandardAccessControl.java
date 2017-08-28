windofthesky/presto

Access control for SHOW ROLE GRANTS and SHOW CURRENT ROLES

presto-hive/src/main/java/com/facebook/presto/hive/security/SqlStandardAccessControl.java
@@ -391,6 +391,16 @@ public class SqlStandardAccessControl
         return roles;
     }
 
+    @Override
+    public void checkCanShowCurrentRoles(ConnectorTransactionHandle transactionHandle, Identity identity, String catalogName)
+    {
+    }
+
+    @Override
+    public void checkCanShowRoleGrants(ConnectorTransactionHandle transactionHandle, Identity identity, String catalogName)
+    {
+    }
+
     private boolean checkDatabasePermission(ConnectorTransactionHandle transaction, Identity identity, String schemaName, HivePrivilege... requiredPrivileges)
     {
         SemiTransactionalHiveMetastore metastore = metastoreProvider.apply(((HiveTransactionHandle) transaction));

presto-plugin-toolkit/src/main/java/com/facebook/presto/plugin/base/security/FileBasedAccessControl.java
@@ -248,6 +248,16 @@ public class FileBasedAccessControl
         return roles;
     }
 
+    @Override
+    public void checkCanShowCurrentRoles(ConnectorTransactionHandle transactionHandle, Identity identity, String catalogName)
+    {
+    }
+
+    @Override
+    public void checkCanShowRoleGrants(ConnectorTransactionHandle transactionHandle, Identity identity, String catalogName)
+    {
+    }
+
     private boolean canSetSessionProperty(Identity identity, String property)
     {
         for (SessionPropertyAccessControlRule rule : sessionPropertyRules) {

sopel39/presto_td

presto-hive/src/main/java/com/facebook/presto/hive/security/SqlStandardAccessControl.java
@@ -384,6 +384,16 @@ public class SqlStandardAccessControl
         return roles;
     }
 
+    @Override
+    public void checkCanShowCurrentRoles(ConnectorTransactionHandle transactionHandle, ConnectorIdentity identity, String catalogName)
+    {
+    }
+
+    @Override
+    public void checkCanShowRoleGrants(ConnectorTransactionHandle transactionHandle, ConnectorIdentity identity, String catalogName)
+    {
+    }
+
     private boolean checkDatabasePermission(ConnectorTransactionHandle transaction, ConnectorIdentity identity, String schemaName, HivePrivilege... requiredPrivileges)
     {
         SemiTransactionalHiveMetastore metastore = metastoreProvider.apply(((HiveTransactionHandle) transaction));

presto-plugin-toolkit/src/main/java/com/facebook/presto/plugin/base/security/FileBasedAccessControl.java
@@ -245,6 +245,16 @@ public class FileBasedAccessControl
         return roles;
     }
 
+    @Override
+    public void checkCanShowCurrentRoles(ConnectorTransactionHandle transactionHandle, ConnectorIdentity identity, String catalogName)
+    {
+    }
+
+    @Override
+    public void checkCanShowRoleGrants(ConnectorTransactionHandle transactionHandle, ConnectorIdentity identity, String catalogName)
+    {
+    }
+
     private boolean canSetSessionProperty(ConnectorIdentity identity, String property)
     {
         for (SessionPropertyAccessControlRule rule : sessionPropertyRules) {